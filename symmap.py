ac809a63aa184f4d873f9b00e2e78fe98e848ceb
Joseph Tompkins <joseph.tompkins@mavs.uta.edu>
2017-10-30 08:13:54 -0500

    Refactored code for readability & modularity.
    Much of the code was written just to get something working. I've refactored the entire file to no longer use global variables. Do not run this script as is right now or it will replace the value_map file with something that will break the ascii_art.py script.

--- symmap.py
+++ symmap.py
@@ -5,38 +5,40 @@ import cv2
 import csv
 
 
-symbol_to_norm_dict = dict() # symbol_to_norm_dict in format:  {ord(symbol): norm value}
-clamped_norm_dict = dict()   # clamped_norm_dict in format:  {ord(symbol): clamped norm value}
-sorted_symbol_list = list()  # sorted by norm values
-value_to_symbol_map = dict() # value_to_symbol_map in format:  {grayscale value: symbol}
+# symbol_to_norm_map format:             {ord(symbol): norm value}
+# clamped_value_to_symbol_dict format:   {ord(symbol): clamped norm value}
+# sorted_symbol_list format:             list of symbols sorted by norm values
+# value_to_symbol_map format:            {int(clamped norm): ord(symbol)}
 
 
-def __build_symbol_to_norm_dict():
+def __build_symbol_to_norm_map(symbols_filepath='./symbols.png'):
     """
     Builds the symbol-to-norm dictionary mapping.
 
     Calculates the norm of each symbol in symbol.png image.
     Black being equal to 0, and white equal to MAX, where MAX is the maximum norm value.
 
-    symbol_to_norm_dict in format:  {ord(symbol): norm value}
+    :return: symbol_to_norm_dict in format:  {ord(symbol): norm value}
     """
-    # symbols.png is an image file composed of ASCII symbols from 32 to 126
-    img = cv2.imread('symbols.png', cv2.IMREAD_GRAYSCALE)
+    # symbols.png is an image file composed of ASCII characters from ordinal values 32 to 126 arranged in a grid
+    # like a "sprite sheet."
+    img = cv2.imread(symbols_filepath, cv2.IMREAD_GRAYSCALE)
     if img.size == 0:
-        print 'symmap.py unable to read symbols.png.'
+        print 'symmap.py unable to read \'' + symbols_filepath + '\'.'
         return
-
+    # create a directory for images of each individual character
     curr_path = os.getcwd()
     curr_path += '/symbols'
     if not os.path.exists(curr_path):
         os.mkdir(curr_path)
-
+    # Divides symbol.png into cells. Cell width and height was derived using an image editor.
     img_dem = img.shape[:2]
-    cell_pixel_height = 17
     cell_pixel_width = 9
-    img_cell_height = img_dem[0] / cell_pixel_height
+    cell_pixel_height = 17
     img_cell_width = img_dem[1] / cell_pixel_width
-
+    img_cell_height = img_dem[0] / cell_pixel_height
+    # iterate cell-by-cell, calculating the vector norm of each cell in the image and storing the
+    symbol_to_norm_dict = dict()
     for i in range(img_cell_height):
         for j in range(img_cell_width):
             if i == img_cell_height - 1 and j == img_cell_width - 1:
@@ -45,101 +47,65 @@ def __build_symbol_to_norm_dict():
             x_pos = j * cell_pixel_width
             cell = img[y_pos:y_pos + cell_pixel_height, x_pos:x_pos + cell_pixel_width]
             cell_num = 32 + (i * 16) + j
-            cv2.imwrite('./symbols/' + str(32 + (i * 16) + j) + '.png', cell)
-
+            cv2.imwrite('./symbols/' + str(32 + (i * 16) + j) + '.png', cell) # creates images of symbols for debugging purposes
+            # cv2.norm Gives the vector norm of the image.
+            # The more black there is in the image, the lower the norm value.
             cell_norm = int(cv2.norm(cell, cv2.NORM_L1))
             symbol_to_norm_dict[cell_num] = cell_norm
 
+    return symbol_to_norm_dict
 
-def __build_sorted_symbol_list():
-    """
-    Builds a list of ascii symbols sorted in order of least-to-greatest norm values and
-    based on the values in the symbol-to-norm dictionary mapping.
-    """
-    for key in symbol_to_norm_dict:
-        i = 0
-        for i in range(len(sorted_symbol_list)):
-            val = symbol_to_norm_dict[key]
-            key2 = sorted_symbol_list[i]
-            val2 = symbol_to_norm_dict[key2]
-
-            if val < val2:
-                break
-
-        sorted_symbol_list.insert(i, key)
-
-    min_key = sorted_symbol_list[0]
-    min_val = symbol_to_norm_dict[min_key]
-    for key in symbol_to_norm_dict:
-        val = symbol_to_norm_dict[key]
-        symbol_to_norm_dict[key] = val - min_val
-
-
-def __build_clamped_norm_dict():
+def __build_clamped_symbol_to_norm_map(symbol_to_norm_map):
     """
     Builds the clamped-norm dictionary mapping, which is a symbol-to-norm mapping where the norm values are clamped
     inclusively between 0 and 255.
 
-    clamped_norm_dict in format:  {ord(symbol): clamped norm value}
+    :returns: clamped_norm_dict in format:  {ord(symbol): clamped norm value}
     """
-    global clamped_norm_dict
-    if symbol_to_norm_dict is None or sorted_symbol_list is None:
-        return
-
-    clamped_norm_dict = symbol_to_norm_dict.copy()
-    max_value = symbol_to_norm_dict[sorted_symbol_list[-1]]
+    clamped_norm_dict = symbol_to_norm_map.copy()
+    norm_values = [symbol_to_norm_map[x] for x in symbol_to_norm_map] # extract norm values from symbol_to_norm_dict
+    max_val = max(norm_values)
+    min_val = min(norm_values)
     for key in clamped_norm_dict:
-        clamped_norm_dict[key] =  int(float(clamped_norm_dict[key]) * (255.0 / max_value))
+        val = symbol_to_norm_map[key]
+        clamped_norm_dict[key] = int((val - min_val) * (255.0 / (max_val - min_val)))
+
+    return clamped_norm_dict
 
 
-def __build_reduced_value_to_symbol_map(even_distributrion=True):
+def __build_value_to_symbol_map(clamped_value_to_symbol_map):
     """
     Builds a reduced value-to-symbol dictionary mapping based on the clamped values of the clamped-norm dictionary.
 
-    Symbols that share norm values with other symbols are eliminated. Each value between 0 and 255 will be mapped
-    to a symbol using one of the provided distributions.
-    :param even_distribution: when True, even distribution is used, else "fill" distribution is used.
+    A value can only be associated with one symbol. If a value already has a mapping to a symbol and both symbols are
+    a special character, the new symbol will become the mapping for the value. Otherwise, no action is taken and
+    the unmapped symbol is ignored. The result is a dictionary where each key represents a value between 0 and 255.
     """
-    global value_to_symbol_map
-    for key in clamped_norm_dict:
-        value = clamped_norm_dict[key]
+    value_to_symbol_map = dict()
+    for key in clamped_value_to_symbol_map:
+        value = clamped_value_to_symbol_map[key]
         if value not in value_to_symbol_map:
-            value_to_symbol_map[value] = chr(key)
+            value_to_symbol_map[value] = key
         else:
             sym = value_to_symbol_map[value]
-            if __is_special_char(sym) and not __is_special_char(key):
-                continue
-            else:
-                value_to_symbol_map[value] = chr(key)
-
-    if even_distributrion:
-        __even_distribution_value_map()
-    else:
-        __fill_distribution_value_map()
+            if __is_special_char(sym) and __is_special_char(key):
+                value_to_symbol_map[value] = key
+    return value_to_symbol_map
 
 
-def __build_reduced_sorted_symbol_list():
+def __build_sorted_symbol_list(value_to_symbol_map):
     """
-    Builds a list of symbols sorted by norm values.
-
-    Only ascii symbols that are in the reduced value-to-symbol map are
-    appended to this list. Ascii symbols that do not have unique clamped-norm-value mappings have been filtered out.
-
-    :return: list containing ascii order integer values sorted by their clamped norm values
+    Builds a list of ascii symbols sorted in order of least-to-greatest norm values and
+    based on the values in the symbol-to-norm dictionary mapping.
     """
-    global value_to_symbol_map
-    sorted_reduced_symbols = list()
-    cur_val = -1
-    for sym in sorted_symbol_list:
-        val = clamped_norm_dict[sym]
-        if val != cur_val:
-            cur_val = val
-            sorted_reduced_symbols.append(ord(value_to_symbol_map[val]))
+    sorted_values = [val for val in value_to_symbol_map]
+    sorted_values.sort()
+    sorted_symbol_list = [value_to_symbol_map[val] for val in sorted_values]
 
-    return sorted_reduced_symbols
+    return sorted_symbol_list
 
 
-def __fill_distribution_value_map():
+def __fill_distribution_value_map(value_to_symbol_map, sorted_symbols_list):
     """
     Distributes value-to-symbol mappings by filling gaps between clamped norm values of symbols.
 
@@ -153,24 +119,22 @@ def __fill_distribution_value_map():
     Gray-scale values 0 through 42 will be split in half, such that values 0 through 20 will be mapped to '@' and
     values 21 trough 42 are mapped to 'Q'.
     """
-    global value_to_symbol_map
-    sorted_reduced_symbols = __build_reduced_sorted_symbol_list()
 
-    for sym in sorted_reduced_symbols:
-        val = clamped_norm_dict[sym]
+    for sym in sorted_symbols_list:
+        val = value_to_symbol_map[sym]
         print '{' + str(val) + ' : ' + chr(sym) + ' = \'' + str(sym) + '\' }'
 
     curr = 0
     for val in range(256):
         if val in value_to_symbol_map:
             print str(val) + ' found in value_to_symbol_map. Symbol: ' + value_to_symbol_map[val]
-            if sorted_reduced_symbols[curr] != ord(value_to_symbol_map[val]):
+            if sorted_symbols_list[curr] != ord(value_to_symbol_map[val]):
                 curr += 1
             continue
         else:
-            curr_sym = sorted_reduced_symbols[curr]
-            next_sym = sorted_reduced_symbols[curr + 1]
-            next_val = clamped_norm_dict[next_sym]
+            curr_sym = sorted_symbols_list[curr]
+            next_sym = sorted_symbols_list[curr + 1]
+            next_val = value_to_symbol_map[next_sym]
             R = range(val, next_val)
             half = int(len(R) / 2) + (val - 1)
             print R
@@ -182,7 +146,7 @@ def __fill_distribution_value_map():
                     value_to_symbol_map[i] = chr(next_sym)
 
 
-def __even_distribution_value_map():
+def __even_distribution_value_map(value_to_symbol_map, sorted_symbols_list):
     """
     Distributes value-to-symbol mappings by giving values equal partitions.
 
@@ -193,19 +157,16 @@ def __even_distribution_value_map():
     e.g. If partition size is determined to be 4 values, each consecutive 4 values in the value-to-symbol map will
     map to the same symbol. Values 0 - 3 map to '@', values 4 - 7 map to 'Q', etc.
     """
-    global value_to_symbol_map
-    sorted_reduced_symbols = __build_reduced_sorted_symbol_list()
+    print 'len(sorted_reduced_symbols) = ' + str(len(sorted_symbols_list))
+    for i in range(len(sorted_symbols_list)):
+        print chr(sorted_symbols_list[i]) + ' :: i = ' + str(i)
 
-    print 'len(sorted_reduced_symbols) = ' + str(len(sorted_reduced_symbols))
-    for i in range(len(sorted_reduced_symbols)):
-        print chr(sorted_reduced_symbols[i]) + ' :: i = ' + str(i)
-
-    sz = len(sorted_reduced_symbols)
+    sz = len(sorted_symbols_list)
     part_sz = int(256 / sz)
     rem = 256 % sz
     val = 0
-    for i in range(len(sorted_reduced_symbols)):
-        sym = sorted_reduced_symbols[i]
+    for i in range(len(sorted_symbols_list)):
+        sym = sorted_symbols_list[i]
         if rem > 0:
             pad_partition = part_sz + 1
             for i in range(pad_partition):
@@ -218,8 +179,7 @@ def __even_distribution_value_map():
                 val += 1
 
 
-def __write_value_map(file_name='value_map'):
-    global value_to_symbol_map
+def __write_value_map(value_to_symbol_map, file_name='value_map'):
     f = open(file_name, 'w')
     writer = csv.DictWriter(f, fieldnames=['value', 'symbol'])
 
@@ -231,10 +191,7 @@ def __write_value_map(file_name='value_map'):
 
 
 def __is_special_char(sym):
-    if sym < 65 or (sym > 90 and sym < 97) or sym > 122:
-        return True
-    else:
-        return False
+    return (sym < 65 or (sym > 90 and sym < 97) or sym > 122)
 
 
 def main():
@@ -242,11 +199,15 @@ def main():
         sym = value_to_symbol_map[val]
         print '{' + str(val) + ' : ' + str(ord(sym)) + ' = \'' + sym + '\' }'
 
-__build_symbol_to_norm_dict()
-__build_sorted_symbol_list()
-__build_clamped_norm_dict()
-__build_reduced_value_to_symbol_map()
-__write_value_map()
+
+symbol_to_norm_map = __build_symbol_to_norm_map()
+clamped_symbol_to_norm_map = __build_clamped_symbol_to_norm_map(symbol_to_norm_map)
+value_to_symbol_map = __build_value_to_symbol_map(clamped_symbol_to_norm_map)
+sorted_symbol_list = __build_sorted_symbol_list(value_to_symbol_map)
+# temporary algorithm to convert the ordinal values of the symbols to actual ascii characters
+for key in value_to_symbol_map:
+    value_to_symbol_map[key] = chr(value_to_symbol_map[key])
+__write_value_map(value_to_symbol_map)
 
 if __name__ == '__main__':
     main()
